8ae1ee39478381815b7b7a954b84a86a4b04e850
Junior <losttides@gmail.com>
2025-07-13 16:31:15 -0400

    1.21.7 (#491)

---
+++ Obsidian.API/Registry/Codecs/Dialogs/DialogAction.cs
@@ -0,0 +1,21 @@
+﻿using Obsidian.API.Utilities;
+using Obsidian.Nbt.Interfaces;
+
+namespace Obsidian.API.Registry.Codecs.Dialogs;
+public sealed record class DialogAction : INbtSerializable
+{
+    public required ChatMessage Label { get; set; }
+
+    public required int Width { get; set; }
+
+    public void Write(INbtWriter writer)
+    {
+        writer.WriteInt("width", Width);
+
+        writer.WriteCompoundStart("label");
+
+        writer.WriteChatMessage(this.Label);
+
+        writer.EndCompound();
+    }
+}
